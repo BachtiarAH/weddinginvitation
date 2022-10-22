20637db0c0575279d456191c313f4345a16dcc83
Abdullah Ali <abdullahali.and1@gmail.com>
2022-10-23 00:13:09 +0700

    menambahkan transisi sampul slide keatas

--- template/bagan/sampul.php
+++ template/bagan/sampul.php
@@ -1,1 +1,4 @@
-<h1 class="nama">Icha & Yasir</h1>
+<div id="awal" class="sampul sampul_belum_pencet" onclick="KeatasSampeIlang()">
+    <h1 class="nama">Icha & Yasir</h1>
+    <button class="pencet">obpencet</button>
+</div>
--- template/komponen/footer.php
+++ template/komponen/footer.php
@@ -1,3 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
 </body>
+
 </html>
