a5d0d0048f5154af6b35ccf5c097d9ed7a9147d0
Abdullah Ali <abdullahali.and1@gmail.com>
2022-10-27 13:58:17 +0700

    asset dan css dari sampul belum selesai

--- template/bagan/sampul.php
+++ template/bagan/sampul.php
@@ -1,4 +1,8 @@
-<div id="awal" class="sampul sampul_belum_pencet" onclick="KeatasSampeIlang()">
-    <h1 class="nama">Icha & Yasir</h1>
-    <button class="pencet">obpencet</button>
+<div class="sampul screen">
+    <div class="overlap-group">
+        <div class="kepada-yth-bapakibusaudarai">Kepada yth<br />bapak/1bu/saudara/i</div>
+        <div class="abdullah-ali ">Abdullah Ali</div>
+        <p class="kami -mengundang-anda ">Kami mengundang anda<br />dalam acara pernikahan</p>
+        <h1 class="icha-yasir ">Icha<br />&amp; <br />Yasir</h1>
+    </div>
 </div>
