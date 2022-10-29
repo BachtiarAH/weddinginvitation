f75494f1badab6669684730c4e009b58a5e13948
BachtiarAH <bachtiarah73@gmail.com>
2022-10-29 08:26:06 +0700

    animasi sudah di perbaiki

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function KeatasSampeIlang() {
+    console.log("clicked");
     document.getElementById('awal').classList.add('sampul_terpencet');
     document.getElementById('awal').classList.remove('sampul_belum_pencet');
 }
--- template/bagan/sampul.php
+++ template/bagan/sampul.php
@@ -1,8 +1,13 @@
-<div class="sampul screen">
-    <div class="overlap-group">
-        <div class="kepada-yth-bapakibusaudarai">Kepada yth<br />bapak/1bu/saudara/i</div>
-        <div class="abdullah-ali ">Abdullah Ali</div>
-        <p class="kami -mengundang-anda ">Kami mengundang anda<br />dalam acara pernikahan</p>
-        <h1 class="icha-yasir ">Icha<br />&amp; <br />Yasir</h1>
+<div class="sampul sampul_belum_pencet" id="awal" onclick="KeatasSampeIlang()">
+    <div class="">
+        <div class="">Kepada yth<br />bapak/1bu/saudara/i</div>
+        <div class="tamu"><?php if (isset($_GET['to'])) {
+                                echo $_GET['to'];
+                            } else {
+                                echo "di Tempat";
+                            }
+                            ?></div>
+        <p class="">Kami mengundang anda<br />dalam acara pernikahan</p>
+        <h1 class="">Icha<br />&amp; <br />Yasir</h1>
     </div>
 </div>
