4952ad1daf8317ad57878fa5fd122b5764254e91
Abdullah Ali <abdullahali.and1@gmail.com>
2022-11-02 22:44:36 +0700

    Kurang spasi, jadi ditambah spasi

--- template/index.php
+++ template/index.php
@@ -4,13 +4,13 @@ include_once('komponen/header.php');
 include_once('bagan/sampul.php');
 ?>
 <div class="uk-position-center">
-<?php
-include_once('template/bagan/home.php');
-include_once('template/bagan/couple.php');
-include_once('template/bagan/event.php');
-include_once('template/bagan/prokes.php');
-include_once('template/bagan/wishes.php');
-?>
+    <?php
+    include_once('template/bagan/home.php');
+    include_once('template/bagan/couple.php');
+    include_once('template/bagan/event.php');
+    include_once('template/bagan/prokes.php');
+    include_once('template/bagan/wishes.php');
+    ?>
 </div>
 <?php
 include_once('komponen/footer.php');
