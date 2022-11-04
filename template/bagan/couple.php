4bc0fb2e007d6d87177f87c8d8f6e55ec961db4b
BachtiarAH <bachtiarah73@gmail.com>
2022-11-04 23:16:39 +0700

    couple done

--- template/bagan/couple.php
+++ template/bagan/couple.php
@@ -2,6 +2,35 @@
     <div class="background-atas"></div>
     <div class="couple-header">We Are Getting Married!</div>
     <div class="bismilah">بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيْم</div>
-    <div class="couple-salam "><p class="shadow">Assalamu’alaikum Warahmatullaahi Wabarakaatuh</p></div>
-    <div class="couple-pembukaan shadow"><p>Maha Suci Allah yang telah menciptakan makhluk-Nya berpasang-pasangan. Ya Allah semoga ridho-Mu tercurahkan mengiringi pernikahan kami.</p></div>
+    <div class="couple-salam Tshadow">
+        <p>Assalamu’alaikum Warahmatullaahi Wabarakaatuh</p>
+    </div>
+    <div class="couple-pembukaan Tshadow">
+        <p>Maha Suci Allah yang telah menciptakan makhluk-Nya berpasang-pasangan. Ya Allah semoga ridho-Mu tercurahkan mengiringi pernikahan kami.</p>
+    </div>
+    <div class="frame-perempuan"></div>
+    <div class="nama-pengantin">
+        <div class="nama Tshadow"><p>Aisyah Dziban</p></div>
+        <div class="detail Tshadow">
+        <p>
+            Putri dari
+
+            Bapak dr. Rusdi Dziban, Sp.B., FINACS (Alm)
+            <br>
+            dan
+
+            Ibu Lina Alfiah Alamudi</p>
+        </div>
+    </div>
+    <div class="spacing-dan">- & -</div>
+    <div class="frame-laki"></div>
+    <div class="nama-pengantin">
+        <div class="nama Tshadow">
+        <p >dr. Muhammad Yasir Masyhadi</p></div>
+        <div class="detail Tshadow">
+            <p >
+                Putra dari Bapak Farid Masyhadi dan <br>Ibu Hikmalia Masyhadi
+            </p>
+        </div>
+    </div>
 </div>
