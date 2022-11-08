81b8a82306b440b64e20fae957d47ce164e264a8
Abdullah Ali <abdullahali.and1@gmail.com>
2022-11-08 16:54:52 +0700

    Revisi

--- template/bagan/wishes.php
+++ template/bagan/wishes.php
@@ -2,23 +2,49 @@
     <div class="teksalign center">
         <br>
         <div class="teksjudul reveal">
-            <p>Wishes</p>
+            <p>Do'a Untuk kedua Mempelai :</p>
         </div>
-        <div class="tekspengertianqs reveal">
-            <p>“And of His signs is that He created for you from <br>
-                yourselves mates that you may find tranquility in <br>
-                them; and he placed between you affection and <br>
-                mercy. indeed in that are signs for a people who give <br>
-                thought.” <br>
+        <div class="teks-wishes reveal">
+            <p>بَارَكَ اللهُ لَكُماَ وَبَارَكَ عَلَيْكُماَ وَجَمَعَ بَيْنَكُمَا
+                <br>فِي خَيْرٍ
+
                 <br>
                 <br>
-                ( QS. Ar- Rum 21 )
+                Baarakallahu lakumaa wa baaraka ‘alaikumaa
                 <br>
-                --
+                wa jama’a bainakumaa fii khair
+                <br>
+                <br>
+                Artinya: Semoga Allah karuniakan barakah pada
+                kalian berdua, dan semoga Ia limpahkan barakah
+                atas kalian berdua, dan semoga Ia himpun kalian
+                berdua dalam kebaikan.
+                <br>
+                (HR. Abu Daud, Tirmidzi, dan Ibnu Majah)
+                <br>
+            <div>
+                <img src="asset/wishes/bunga-tengah.png" alt="bunga-tengah" width="290" height="20">
+            </div>
+            <br>
+            وَمِنۡ اٰيٰتِهٖۤ اَنۡ خَلَقَ لَكُمۡ مِّنۡ اَنۡفُسِكُمۡاَزۡوَاجًا لِّتَسۡكُنُوۡۤا اِلَيۡهَا وَجَعَلَ بَيۡنَكُمۡ مَّوَدَّةًوَّرَحۡمَةً  ؕ اِنَّ فِىۡ ذٰ لِكَ لَاٰيٰتٍ لِّقَوۡمٍ يَّتَفَكَّرُوۡنَ
+            <br>
+            <br>
+            “Dan di antara tanda-tanda kekuasaan-Nya
+            ialah Dia menciptakan untukmu istri-istri dari
+            jenismu sendiri, supaya kamu cenderung
+            dan merasa tentram kepadanya, dan dijadikan-Nya
+            di antaramu rasa kasih dan sayang.
+            Sesungguhnya pada yang demikian itu benar-benar
+            terdapat tanda-tanda bagi kaum yang berpikir.”
+            <br>
+            <br>
+            (QS Ar-Rum: 21)
+            <br>
+            <br>
             </p>
         </div>
         <div class="kotak tengah reveal">
-            <img src="asset\wishes\fotoberduailus.png" alt="foto berdua">
+            <img src="asset\wishes\fotoberduailus.png" alt="foto berdua" width="230" height="235">
         </div>
         <br>
         </br>
